e964265d3ec4d02cb2a79fada2cfcf753ba5900e
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2018-01-29 18:53:08 +0700

    Async program

---
+++ AsyncProgramming/AsyncTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    public  class AsyncTask
+    {
+        public  async Task<int> GetContentLengthAsync(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            Task<String> getStringTask = httpClient.GetStringAsync(url);
+
+            DoIndepenceWork();
+
+            string content = await getStringTask;
+
+            return  content.Length;
+        }
+
+        private void DoIndepenceWork()
+        {
+            Console.WriteLine("Continue other work in async");
+        }
+    }
+}
---
+++ AsyncProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://msdn.microsoft.com/en-us/";
+            AsyncTask async = new AsyncTask();
+            Task<int> result = async.GetContentLengthAsync(url);
+            Console.WriteLine(result.Result);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
